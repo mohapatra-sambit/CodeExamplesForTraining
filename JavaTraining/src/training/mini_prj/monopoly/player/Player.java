f6c9f27bcd5cdcf507d1af1c5a0324e854d60870
Sambit Mohapatra <sambit.mohapatra@gmail.com>
2025-01-20 16:38:53 +0530

    mini-prj update

--- JavaTraining/src/training/mini_prj/monopoly/player/Player.java
+++ JavaTraining/src/training/mini_prj/monopoly/player/Player.java
@@ -54,7 +54,22 @@ public class Player {
 	}
 	
 	public void play() {
-		/////////
+		// 1. Invoke Dice class's roll method
+		// 2. Add the dice value to the current position of the player.
+		//     That sets the new current position of the player.
+		// 3. If the new position is a place,
+		//     a. Use the CellOwner, for the following logic. 
+		//	   b. If it is already owned by someone else, determine the owner and the rent amount.
+		//	   		i. Subtract the rent amount from the current player
+		//	   		ii. Add the rent amount to the player who owns this place.
+		//	   c. If the place is available to buy,
+		//	   		i. If current player has sufficient money to buy the place
+		//				* Subtract the place amount from the current player's account.
+		//				* Invoke the current player's buyProperty method
+		//				* Update CellOwner class, about this new ownership.
+		//			ii. If not, current player has to pay $25 as fine.
+		// 4. Same logic will apply for Railway and Utility.
+		// 5. If Tax, then simply reduce the tax amount from the current's player.
 	}
 
 	@Override
